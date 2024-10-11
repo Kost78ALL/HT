ad2a3b4a7c824c320eb6d28765764f1422beb68b
Lex <79052688528@yandex.ru>
2024-10-11 20:55:49 +0300

    M_1_4

--- Dopik.py
+++ Dopik.py
@@ -1,2 +1,7 @@
-Stud={'Alex','Toha','Miha','Serg',}
+Stud={'Alex','Toha','Miha','Serg','Kolyan'}
 print(Stud,'-Список студентов')
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+s=sorted(list(Stud))
+res=[sum(grades[i])/len(grades[i]) for i in range(len(grades))]
+Av_res={s[y]:res[y] for y in range(len(s))}
+print(Av_res,'- Средние баллы')
